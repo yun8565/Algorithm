b0e96ce1333c0aeb371eb99a60578c12f7bad4a3
Changyun Lee <86460086+yun8565@users.noreply.github.com>
2022-07-05 15:13:23 +0900

    [Silver III] Title: 퇴사, Time: 208 ms, Memory: 17812 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -3,34 +3,23 @@ import java.util.*;
 public class Main {
     static int N;
 
-    static int[] time;
-    static int[] profit;
-    static int[] dp;
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-
         N = s.nextInt();
 
-        time = new int[N+1];
-        profit = new int[N+1];
-        dp = new int[N+1];
+        int[] time = new int[N+1];
+        int[] profit = new int[N+1];
+        int[] dp = new int[N+1];
 
         for(int i = 1; i <= N; i++) {
             time[i] = s.nextInt();
             profit[i] = s.nextInt();
-        }
-
-        consult();
-        Arrays.sort(dp);
-        System.out.print(dp[N]);
-    }
-
-    public static void consult(){
-        for(int i = 1; i <= N; i++) {
+            
             if(time[i] <= N-i+1)
                 for (int j = 1; j <= i; j++)
                     dp[i] = time[i-j] <= j ? Math.max(dp[i], profit[i] + dp[i-j]) : Math.max(dp[i], profit[i]);
         }
+        Arrays.sort(dp);
+        System.out.print(dp[N]);
     }
 }
