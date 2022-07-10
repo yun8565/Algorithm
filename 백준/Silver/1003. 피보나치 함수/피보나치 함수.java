ae5fd4a1a19ac5b0afc58a423bdb1aa9de9fa8e9
Changyun Lee <86460086+yun8565@users.noreply.github.com>
2022-07-10 23:27:13 +0900

    [Silver III] Title: 피보나치 함수, Time: 236 ms, Memory: 18464 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,38 @@
+import javax.sound.midi.SysexMessage;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] D0;
+    static int[] D1;
+    static int N;
+
+    public static void main(String[] args) {
+        //BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        Scanner s = new Scanner(System.in);
+        D0 = new int[41];
+        D1 = new int[41];
+
+        D0[0] = 1;
+        D1[0] = 0;
+        D0[1] = 0;
+        D1[1] = 1;
+
+        int T = s.nextInt();
+        for(int i = 0; i < T; i++) {
+            N = s.nextInt();
+            if(N != 0 && N != 1)
+                fibonacci(0);
+            System.out.println(D0[N]+" "+D1[N]);
+        }
+    }
+
+    static void fibonacci(int n) {
+        if(n == N+1) return;
+        if (D0[n] == 0 && D1[n] == 0) {
+            D0[n] = D0[n - 1] + D0[n - 2];
+            D1[n] = D1[n - 1] + D1[n - 2];
+        }
+        fibonacci(n+1);
+    }
+}
