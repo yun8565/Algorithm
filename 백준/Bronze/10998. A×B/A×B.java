547365609495815861e3b2d69d6e3e0e9d1b7721
Changyun Lee <86460086+yun8565@users.noreply.github.com>
2023-06-14 18:43:10 +0900

    [Bronze V] Title: A×B, Time: 212 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        Scanner s = new Scanner(System.in);
+
+        int A, B;
+        A = s.nextInt();
+        B = s.nextInt();
+        
+        System.out.println(A*B);
+    }
+}
