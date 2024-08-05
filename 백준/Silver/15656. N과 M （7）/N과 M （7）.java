05b9b9bbb0a75eb761fe374062e0a6519544a105
Changyun Lee <86460086+yun8565@users.noreply.github.com>
2024-08-05 09:34:46 +0900

    [Silver III] Title: N과 M (7), Time: 660 ms, Memory: 142980 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static List<Integer> list = new ArrayList<>();
+    static int N,M;
+    static int[] num;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        num = new int[N];
+
+        String[] numbers = br.readLine().split(" ");
+        for(int i = 0; i < N; i++)
+            num[i] = Integer.parseInt(numbers[i]);
+        Arrays.sort(num);
+
+        dfs();
+        System.out.print(sb.toString());
+    }
+
+    static void dfs() {
+        if(list.size() == M) {
+            for(int i : list)
+                sb.append(i).append(" ");
+            sb.append("\n");
+            return;
+        }
+        for(int i = 0 ; i < N ; i++) {
+            list.add(num[i]);
+            dfs();
+            list.remove(list.size()-1);
+        }
+    }
+}
