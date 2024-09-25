e0e1db98b5d6fb802866c470b8bdb06971be111f
Changyun Lee <86460086+yun8565@users.noreply.github.com>
2024-09-25 11:00:44 +0900

    [Gold V] Title: 문자열 게임 2, Time: 408 ms, Memory: 49100 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,20 +1,22 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-
-        int T = s.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Integer,List<Integer>> check = new HashMap<>();
+        
+        int T = Integer.parseInt(br.readLine());
 
         while(T-- > 0) {
-            char[] W = s.next().toCharArray();
-            int K = s.nextInt();
-
-            HashMap<Integer,List<Integer>> check = new HashMap<>();
+            check.clear();
             int min = Integer.MAX_VALUE;
             int max = Integer.MIN_VALUE;
-
+            
+            char[] W = br.readLine().toCharArray();
+            int K = Integer.parseInt(br.readLine());
+            
             for(int i = 0; i < W.length; i++) {
                 int index = W[i]-'0';
                 check.putIfAbsent(index, new ArrayList<>());
@@ -24,11 +26,11 @@ public class Main {
             for(Map.Entry<Integer, List<Integer>> e : check.entrySet()) {
                 List<Integer> list = e.getValue();
                 if(list.size() >= K) {
-                    int rear = 0;
-                    for(int front = 0; front < list.size(); front++) {
+                    for(int rear = 0, front = 0; front < list.size(); front++) {
                         if(front - rear == K-1) {
-                            min = Math.min(min, list.get(front)-list.get(rear)+1);
-                            max = Math.max(max, list.get(front)-list.get(rear++)+1);
+                            int len = list.get(front) - list.get(rear++) + 1;
+                            min = Math.min(min, len);
+                            max = Math.max(max, len);
                         }
                     }
                 }
