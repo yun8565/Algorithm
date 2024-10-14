2b242899f5eed63261c7e10bcbc10bad4db940d4
Changyun Lee <86460086+yun8565@users.noreply.github.com>
2024-10-14 17:37:18 +0900

    [Gold V] Title: LCS, Time: 136 ms, Memory: 18568 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -7,18 +7,16 @@ public class Main {
 
         String str1 = br.readLine();
         String str2 = br.readLine();
-        int n = str1.length();
-        int m = str2.length();
-        int[][] dp = new int[n+1][m+1];
+        int[][] dp = new int[str1.length()+1][str2.length()+1];
 
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
+        for(int i = 1; i <= str1.length(); i++) {
+            for(int j = 1; j <= str2.length(); j++) {
                 if(str1.charAt(i-1) == str2.charAt(j-1))
                     dp[i][j] = dp[i-1][j-1] + 1;
                 else
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
-        System.out.println(dp[n][m]);
+        System.out.println(dp[str1.length()][str2.length()]);
     }
 }
