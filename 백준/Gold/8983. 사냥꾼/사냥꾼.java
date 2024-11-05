0a78c3663c2ec95d93b34314e3ea4730526e0c71
Changyun Lee <86460086+yun8565@users.noreply.github.com>
2024-11-05 16:42:48 +0900

    [Gold IV] Title: 사냥꾼, Time: 600 ms, Memory: 53380 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.java
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.java
@@ -23,18 +23,20 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            
+
             int left = 0, right = shootingSpots.length-1;
             while(left <= right) {
                 int mid = (left + right)/2;
-                int dist = Math.abs(shootingSpots[mid]-x) + y;
-                
+                long dist = Math.abs(shootingSpots[mid]-x) + y;
+
                 if(dist <= L) {
                     catchable++;
-                    right = mid - 1;
+                    break;
                 }
-                if(dist > L)
+                if(shootingSpots[mid] <= x)
                     left = mid + 1;
+                if(shootingSpots[mid] > x)
+                    right = mid - 1;
             }
         }
         System.out.println(catchable);
